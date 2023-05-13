2eda9f6253181a0064338f6a32f0a918915249cc
mr-abims <adebayoabimbola9@gmail.com>
2023-05-13 14:15:38 +0100

    chore: forge init

---
+++ Smart-contract/test/Purse.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Purse.sol";
+
+contract CounterTest is Test {
+    PurseContract public purse;
+
+    function setUp() public {
+        // purse = new PurseContract();
+        
+    }
+
+    
+}
